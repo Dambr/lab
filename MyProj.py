03e8a8afe5590e5e2fd7934bd60b5f8140a0f072
Dambr <dambr-wolf@mail.ru>
2019-03-17 09:54:45 +0300

    v 1.0

---
+++ MyProj.py
@@ -0,0 +1,31 @@
+from PIL import Image, ImageDraw
+import threading
+import time
+
+image = Image.open("C:\\Users\\asus\\Desktop\\holst.jpg")
+print("Начальное изображение")
+print()
+image.show()
+
+def run(i):
+	threading.currentThread().getName()
+	for j in range(255):
+		image.putpixel((j, i), (i, j, 0))
+now = time.time()
+
+for i in range(255):
+	for j in range(255):
+		image.putpixel((j, i), (i, j, 255))
+
+image.show()
+print("Время без потоков:", round(time.time()-now, 2), "с")
+
+
+now = time.time()
+for i in range(255):
+	my_thread = threading.Thread(target = run, args=(i,))
+	my_thread.start()
+print()
+print("Время c потоками:", round(time.time()-now, 2), "с")
+image.show()
+input()
