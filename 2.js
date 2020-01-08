49dd1a4aecaca22ff88999dbc4efd3facd75b101
Dambr <dambr@MacBook-Air-Dambr.local>
2020-01-08 15:39:48 +0300

    v1

---
+++ 2.js
@@ -0,0 +1,29 @@
+let N = 10;
+let count = 0;
+
+function isPresent(x){
+	let max = Math.sqrt(x);
+	for (let i = 2; i <= max; i ++){
+		if (x % i == 0){
+			return false;
+		}
+	}
+	return true;
+}
+
+function nextPresent(x){
+	let step = 1;
+	while(true){
+		if ( isPresent(x + step) ){
+			return x + step;
+		}
+		step ++;
+	}
+}
+
+let number = nextPresent(1);
+
+while(number < N){
+	console.log(number);
+	number = nextPresent(number);
+}
